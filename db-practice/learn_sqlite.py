ae2b8ef36d42299dcd8f21bfd60daef9950c7721
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-08-04 13:03:47 -0300

    Agregro practica de insertar datos a la tabla

--- db-practice/learn_sqlite.py
+++ db-practice/learn_sqlite.py
@@ -7,12 +7,29 @@ conn = sqlite3.connect("mi_base_practica.db")
 # 4. Create a cursor (que es un cursor?)
 cur = conn.cursor()
 
+# 6. Test Data
+names_list = [
+    ("Roderick", "Watson"),
+    ("Roger", "Hom"),
+    ("Petri","Halonen"),
+    ("Jussi",""),
+    ("James","McCann")
+]
+
 # 5. Create a 'people' table
 '''Se usa el cursor para ejecutar el SQL query
 Se puede llamar al metodo Execute directamente en el objeto Connection, este obejeto crea un Cursor y lo usa para ejecutar el query, tambien retorna el cursor para acceso a datos'''
 cur.execute('''CREATE TABLE IF NOT EXISTS people
             (first_name TEXT, last_name TEXT)''')
 conn.commit()   # commit this query 
+
+# 7. Insert data into database
+#### que hace el (?,?) ??
+#### para el metodo 'executemany' el segundo argumento debe ser un objeto iterable
+cur.executemany('''
+        INSERT INTO people (first_name, last_name) VALUES (?,?)
+''', names_list)
+conn.commit()
 
 # 3. ALWAYS close db objects/connection           (why?)
 cur.close()
